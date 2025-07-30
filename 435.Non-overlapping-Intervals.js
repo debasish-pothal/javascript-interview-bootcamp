3b03a06a10304d9f00d9ccc947cd60a9fc24cd4f
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-30 18:19:05 +0530

    Create 435.Non-overlapping-Intervals.js

---
+++ 435.Non-overlapping-Intervals.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function (intervals) {
+  if (intervals.length === 0) {
+    return 0;
+  }
+
+  let count = 0;
+  intervals.sort((a, b) => a[1] - b[1]);
+  let end = intervals[0][1];
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] < end) {
+      count++;
+    } else {
+      end = intervals[i][1];
+    }
+  }
+
+  return count;
+};
