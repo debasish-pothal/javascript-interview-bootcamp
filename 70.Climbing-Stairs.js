cb3582558d5af4b9d566c51b1bfcbf3807025ae7
Debasish Pothal <debasish_pothal@hotmail.com>
2025-08-04 20:38:25 +0530

    Update 70.Climbing-Stairs.js

--- 70.Climbing-Stairs.js
+++ 70.Climbing-Stairs.js
@@ -3,11 +3,18 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  const result = [1, 2];
-
-  for (let i = 2; i < n; i++) {
-    result[i] = result[i - 1] + result[i - 2];
+  if (n <= 2) {
+    return n;
   }
 
-  return result[n - 1];
+  let prev = 1;
+  let current = 2;
+
+  for (let i = 2; i < n; i++) {
+    const temp = current + prev;
+    prev = current;
+    current = temp;
+  }
+
+  return current;
 };
